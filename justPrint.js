f9adfc586010ee5094d84f9ff5fc9fc338403842
juniuszhou <junius.zhou@gmail.com>
2015-06-08 02:06:57 -0700

    merge master

---
+++ justPrint.js
@@ -0,0 +1,18 @@
+var a = "hello"
+var b = 100
+var c = function(p1, p2) {
+	return p1 + p2
+}
+
+function hell() {
+	console.log("hello")
+}
+
+hell()
+
+console.log("hello world.")
+console.log(a)
+console.log(b)
+console.log(c(a,b))
+
+
