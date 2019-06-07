7cea27fca8203e2b6ed1a75fcac1d80b2369caae
juniuszhou <junius.zhou@gmail.com>
2019-06-07 16:15:44 +0800

    first react hello world.

--- proj_react/build/server.js
+++ proj_react/build/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const proxy = require('http-proxy-middleware')
+var routes = require('../routes/index');
+var users = require('../routes/users');
 
 /*const options = {
     target: "http://localhost:8080",
@@ -10,7 +12,13 @@ const apiProxy = proxy(options)*/
 
 const app = express()
 
+
+
 app.use(express.static(__dirname))
 //app.use('/', apiProxy)
+
+// app.use('/', routes);
+// app.use('/users', users);
+
 app.listen(80)
 
