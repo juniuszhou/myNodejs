d47033add551b17b5f3e441f7599ba3e58091f3a
juniuszhou <junius.zhou@gmail.com>
2019-06-07 17:55:40 +0800

    delete item

--- proj_react/src/App.js
+++ proj_react/src/App.js
@@ -4,7 +4,8 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: ['learn react', 'learn english']
+            list: ['learn react', 'learn english'],
+            inputValue: ''
         }
     }
 
@@ -12,9 +13,27 @@ class App extends React.Component {
     handleBtnClick() {
         // you must use react pre-defined method to update class data members.
         // then react can render again.
+        console.log(this.state.inputValue)
         this.setState({
-            list: [...this.state.list, 'Hello']
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
         });
+
+    }
+
+    handleInputChange(e) {
+
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    handleOnItemClick(index) {
+        const newList = this.state.list
+        newList.splice(index, 1)
+        this.setState({
+            list: newList
+        })
     }
 
     render() {
@@ -24,14 +43,14 @@ class App extends React.Component {
                     hello, I am junius {2 * 3}.
                 </div>
                 <div>
-                    <input/>
-                     {/*this need rebind to button's parent class's object.*/}
-                     {/*then you can update state.list defined in App.*/}
+                    <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
+                    {/*this need rebind to button's parent class's object.*/}
+                    {/*then you can update state.list defined in App.*/}
                     <button onClick={this.handleBtnClick.bind(this)}> add</button>
                 </div>
                 <ul>
                     {this.state.list.map((item, index) => {
-                            return <li key={index}>{item}</li>
+                            return <li key={index} onClick={this.handleOnItemClick.bind(this, index)}>{item}</li>
                         }
                     )}
                 </ul>
