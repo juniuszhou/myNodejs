540dd717846de7e9ac1ea6c078eeb6fa83a830fe
juniuszhou <junius.zhou@gmail.com>
2019-06-07 16:58:40 +0800

    dynamic add item.

--- proj_react/src/App.js
+++ proj_react/src/App.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div >
-       hello, I am junius.
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: ['learn react', 'learn english']
+        }
+    }
+
+    // define method for button click.
+    handleBtnClick() {
+        // you must use react pre-defined method to update class data members.
+        // then react can render again.
+        this.setState({
+            list: [...this.state.list, 'Hello']
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    hello, I am junius {2 * 3}.
+                </div>
+                <div>
+                    <input/>
+                     {/*this need rebind to button's parent class's object.*/}
+                     {/*then you can update state.list defined in App.*/}
+                    <button onClick={this.handleBtnClick.bind(this)}> add</button>
+                </div>
+                <ul>
+                    {this.state.list.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        }
+                    )}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
